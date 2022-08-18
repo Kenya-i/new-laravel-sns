91c0a47e25fcc637487727a6d12566895835021a
Kenya Ishizuka <ken8116@icloud.com>
2022-08-18 23:57:22 +0900

    index.blade.php

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -12,16 +12,16 @@
           ユーザー名
         </div>
         <div class="font-weight-lighter">
-          2020/2/1 12:00
+          
         </div>
       </div>
     </div>
     <div class="card-body pt-0 pb-2">
       <h3 class="h4 card-title">
-        記事タイトル
+
       </h3>
       <div class="card-text">
-        記事本文
+
       </div>
     </div>
   </div>
